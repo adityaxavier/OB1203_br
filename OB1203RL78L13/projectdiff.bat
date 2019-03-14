f0849a1b56daca01cd6f7d904b103b28eb9f7dc1
Onkar Raut <onkar.raut@renesas.com>
2019-03-14 16:42:27 -0700

    Added missing script file.

---
+++ OB1203RL78L13/projectdiff.bat
@@ -0,0 +1,4 @@
+TortoiseMerge.exe /origfile:".\IDT_Latest\SPO2.cpp" /modifiedfile:".\SPO2\SPO2.cpp" /createunifieddiff /outfile:".\IDT_Latest\SPO2_cpp.patch"
+TortoiseMerge.exe /origfile:".\IDT_Latest\SPO2.h" /modifiedfile:".\SPO2\SPO2.h" /createunifieddiff /outfile:".\IDT_Latest\SPO2_h.patch"
+TortoiseMerge.exe /origfile:".\IDT_Latest\OB1203.cpp" /modifiedfile:".\OB1203\OB1203.cpp" /createunifieddiff /outfile:".\IDT_Latest\OB1203_cpp.patch"
+TortoiseMerge.exe /origfile:".\IDT_Latest\OB1203.h" /modifiedfile:".\OB1203\OB1203.h" /createunifieddiff /outfile:".\IDT_Latest\OB1203_h.patch"
